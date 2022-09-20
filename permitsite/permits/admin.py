94dde4a5eba9a6c082e10ad881d67f052aa81859
Rinat <angrykeo@gmail.com>
2022-09-20 22:17:57 +0300

    Initial commit

---
+++ permitsite/permits/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from permits.models import Permit
+
+admin.site.register(Permit)
---
+++ permitsite/permits/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Permit(models.Model):
+    number = models.PositiveIntegerField()
+    car_number = models.CharField(max_length=15)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
